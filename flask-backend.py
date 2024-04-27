50918e119bd3feac855e180543c41462d439d8b2
ahmad-sm02 <66691193+ahmad-sm02@users.noreply.github.com>
2024-04-28 01:30:17 +0400

    API backend

--- flask-backend.py
+++ flask-backend.py
@@ -6,55 +6,59 @@ class Profile:
     Represents a user profile.
 
     Attributes:
-        userID (int): The unique identifier of the user.
+        user_id (int): The unique identifier of the user.
         name (str): The name of the user.
-        username (str): The username of the user.
         follower_of (str): The user that this profile follows.
-        declared_location (str): The location declared by the user.
         tweets_count (int): The number of tweets made by the user.
-        followers_count (int): The number of followers of the user.
-        friend_count (int): The number of friends of the user.
         score (float): The score of the user profile from 0 to 100 (default is -1.0).
         posts (list): A list of posts made by the user (default is empty).
     """
 
-    def __init__(self, user_id, name, username, follower_of, declared_location, tweets_count, followers_count, friend_count):
+    def __init__(self, user_id, name, follower_of, tweets_count):
+        """
+        Initializes a new instance of the Profile class.
+
+        Args:
+            user_id (int): The unique identifier of the user.
+            name (str): The name of the user.
+            follower_of (str): The user that this profile follows.
+            tweets_count (int): The number of tweets made by the user.
+        """
         self.user_id = user_id
         self.name = name
-        self.username = username
         self.follower_of = follower_of
-        self.declared_location = declared_location
         self.tweets_count = tweets_count
-        self.followers_count = followers_count
-        self.friend_count = friend_count
-        self.score = -1.0; # -1 is an invalid score and means that the score has not been calculated yet
+        self.score = -1.0  # -1 is an invalid score and means that the score has not been calculated yet
         self.posts = []
         self.posts_true_or_not = []
-        
-    def create_profiles_from_database(profiles_database, posts_database):
+
+    @staticmethod
+    def create_profiles_from_database(database):
         """
         Creates and populates profiles from a database.
+
+        Args:
+            database (list): A list of dictionaries representing profile data from the database.
 
         Returns:
             None
         """
         # Iterate over the database data and create profile objects
-        for data in profiles_database:
+        for data in database:
             user_id = data.get('user_id')
-            name = data.get('name')
-            username = data.get('username')
-            follower_of = data.get('follower_of')
-            declared_location = data.get('declared_location')
-            tweets_count = data.get('tweets_count')
-            followers_count = data.get('followers_count')
-            friend_count = data.get('friend_count')
-            profile = Profile(user_id, name, username, follower_of, declared_location, tweets_count, followers_count, friend_count)
-            profiles.append(profile)
-            
+            if not user_exists(user_id):
+                name = data.get('name')
+                follower_of = data.get('follower_of')
+                tweets_count = data.get('tweets_count')
+                profile = Profile(user_id, name, follower_of, tweets_count)
+                profiles.append(profile)
+            profile.posts.append(data.get('text'))
 
-        # Determine if the posts are true or not
-        for post in profile.posts:
-            true_or_not(post)
+        # Determine if the posts are true or not by running the model
+        for profile in profiles:
+            for post in profile.posts:
+                profile.posts_true_or_not.append(true_or_not(post))
+            profile.score = calculate_score(profile)
 
         return
 
@@ -69,11 +73,11 @@ def user_exists(user_id):
         user_id (int): The ID of the user to check.
 
     Returns:
-        bool: True if a user with the given user_id exists, False otherwise.
+        Union[Profile, bool]: The profile object if a user with the given user_id exists, False otherwise.
     """
     for profile in profiles:
         if profile.user_id == user_id:
-            return True
+            return profile
     return False
 
 def get_user_score(user_id):
@@ -84,12 +88,25 @@ def get_user_score(user_id):
     user_id (int): The ID of the user.
 
     Returns:
-    int: The score of the user. Returns -1 if the user ID is not found.
+    int: The score of the user. Returns -1.0 if the user ID is not found.
     """
     for profile in profiles:
         if profile.user_id == user_id:
             return profile.score
-    return -1
+    return -1.0
+
+def true_or_not(post):
+    # TODO: Implement function that uses model to determine if a post is true or not
+    return
+
+def calculate_score(profile):
+    # TODO: Implement function that calculates the score of a profile
+    return
+
+def new_post(post_content, profile):
+    profile.posts.append(post_content)
+    profile.posts_true_or_not.append(true_or_not(post_content))
+    profile.score = calculate_score(profile)
 
 @app.route('/getScore', methods=['GET'])
 def get_score():
@@ -97,22 +114,22 @@ def get_score():
     Retrieves the score for a user based on the provided data.
 
     Returns:
-        int: The score of the user (Adds given profile and returns -1.0 if no score available).
+        int: The score of the user (Adds given profile and returns -1.0 if user does not exist).
     """
     data = request.get_json()
     user_id = data.get('user_id')
-    if(user_exists(user_id)):
+    if user_exists(user_id):
         return get_user_score(user_id)
-    name = data.get('name')
-    username = data.get('username')
-    follower_of = data.get('follower_of')
-    declared_location = data.get('declared_location')
-    tweets_count = data.get('tweets_count')
-    followers_count = data.get('followers_count')
-    friend_count = data.get('friend_count')
-    profile = Profile(user_id, name, username, follower_of, declared_location, tweets_count, followers_count, friend_count)
-    profiles.append(profile)
-    return -1.0
+    else:
+        # If user not found, add the user to the profiles list
+        name = data.get('name')
+        follower_of = data.get('follower_of')
+        tweets_count = data.get('tweets_count')
+        post_content = data.get('text')
+        profile = Profile(user_id, name, follower_of, tweets_count)
+        profiles.append(profile)
+        new_post(post_content, profile)
+        return -1.0
 
 @app.route('/addPost', methods=['POST'])
 def add_post():
@@ -128,9 +145,7 @@ def add_post():
 
     for profile in profiles:
         if profile.user_id == user_id:
-            profile.posts.append(post_content)
-            
-            calculate_score(profile)
+            new_post(post_content, profile)
             return "Post added successfully."
 
     return "User not found."
